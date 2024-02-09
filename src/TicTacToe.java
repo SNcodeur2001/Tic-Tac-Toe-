46b3797490f93c3a68c58a96a869a9dffb4d9059
Mapathe Ndiaye <mapathendiaye542@gmail.com>
2024-02-09 10:55:07 +0000

    Ajout de l'etiquette et du panneau

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -9,13 +9,34 @@ public class TicTacToe {
 
     //Creation de la fenetre 
     JFrame frame = new JFrame();
-    
+
+    //Creation de l'etiquette 
+    JLabel textLabel = new JLabel();
+    //Creation du panneau
+    JPanel textPanel = new JPanel();
+
+
     TicTacToe(){
+        //Propriétés de la fenetre
         frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
-        frame.setLocation(null);
+        frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+
+                //Propriétés de l'etiquette
+
+        textLabel.setBackground(Color.darkGray);
+        textLabel.setForeground(Color.white);
+        textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Tic-Tac-Toe");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
     }
 }
