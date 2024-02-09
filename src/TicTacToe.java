b9472a639d3d68d5b00818ed53ee219f5bd106a2
Mapathe Ndiaye <mapathendiaye542@gmail.com>
2024-02-09 11:14:20 +0000

    Ajout des bords et des boutons

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,13 +8,22 @@ public class TicTacToe {
     int boardHeight = 650;// 50 px pour le Panel 
 
     //Creation de la fenetre 
-    JFrame frame = new JFrame();
+    JFrame frame = new JFrame("Tic-Tac-Toe");
 
     //Creation de l'etiquette 
     JLabel textLabel = new JLabel();
     //Creation du panneau
     JPanel textPanel = new JPanel();
 
+    //Creation des bords
+    JPanel boardPanel = new JPanel();
+
+    //Creation des boutons
+
+    JButton[][] board = new JButton[3][3];
+    String playerX = "X";
+    String playerO  = "O";
+    String currentplayer = playerX;  
 
     TicTacToe(){
         //Propriétés de la fenetre
@@ -38,5 +47,17 @@ public class TicTacToe {
         textPanel.add(textLabel);
         frame.add(textPanel, BorderLayout.NORTH);
 
+
+        boardPanel.setLayout(new GridLayout(3,3));
+        boardPanel.setBackground(Color.darkGray);
+        frame.add(boardPanel);
+
+        for (int r = 0; r < 3; r++) {
+            for(int c = 0; c<3; c++){
+                JButton tile = new JButton();
+                board[r][c] =tile;
+                boardPanel.add(tile);
+            }
+        }
     }
 }
