a8cad31fccff98772ae5a3435aa55020f0cc9385
Mapathe Ndiaye <mapathendiaye542@gmail.com>
2024-02-09 12:13:34 +0000

    Ajout de la lagique du jeu de Tic-Tac-Toe

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -25,6 +25,10 @@ public class TicTacToe {
     String playerO  = "O";
     String currentplayer = playerX;  
 
+    boolean gameOver = false;
+    int turns = 0;
+
+
     TicTacToe(){
         //Propriétés de la fenetre
         frame.setVisible(true);
@@ -66,15 +70,103 @@ public class TicTacToe {
 
                 tile.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent e){
+                        if (gameOver) return;
                         JButton tile = (JButton) e.getSource();
-                        tile.setText(currentplayer); 
-
-                        // alterner les joueurs 
-                        currentplayer = currentplayer == playerX ? playerO : playerX;
-                        textLabel.setText("Tour de "+currentplayer);
+                        if (tile.getText() == "") {
+                            tile.setText(currentplayer);
+                            turns++; 
+                            checkWinner();
+                            if(!gameOver){
+                                 // alterner les joueurs 
+                            currentplayer = currentplayer == playerX ? playerO : playerX;
+                            textLabel.setText("Tour de "+currentplayer); 
+                            }
+                           
+                        }
+                        
                     }
                 });
             }
         }
+    }
+
+    void checkWinner(){
+        //Horizontale
+        for (int r = 0; r < 3; r++) {
+            if(board[r][0].getText() == "") continue;
+
+            if(board[r][0].getText() == board[r][1].getText()&&
+            board[r][1].getText() == board[r][2].getText()){
+
+                for (int i = 0; i < 3; i++) {
+                    setWinner(board[r][i]);
+                }
+                gameOver =true;
+                return;
+            }
+        }
+
+        //vertical
+
+        for (int c = 0; c < 3; c++) {
+            if(board[0][c].getText() == "") continue;
+
+            if(board[0][c].getText() == board[1][c].getText()&&
+            board[1][c].getText() == board[2][c].getText()){
+
+                for (int i = 0; i < 3; i++) {
+                    setWinner(board[i][c]);
+                }
+                gameOver =true;
+                return;
+            }
+        }
+        // Diagonale
+       if(board[0][0].getText() == board[1][1].getText() &&
+       board[1][1].getText() == board[2][2].getText() &&
+       board[0][0].getText()!=""){
+
+        for (int i = 0; i < board.length; i++) {
+            setWinner(board[i][i]);
+        }
+        gameOver=true;
+        return;
+       }
+
+       //anti-Diagonale
+       if(board[0][2].getText() == board[1][1].getText() &&
+       board[1][1].getText() == board[2][0].getText() &&
+       board[0][2].getText()!=""){
+
+        setWinner(board[0][2]);
+        setWinner(board[1][1]);
+
+        setWinner(board[2][0]);
+
+
+        gameOver=true;
+        return;
+       }
+       if (turns == 9) {
+            for (int r = 0; r < board.length; r++) {
+              for (int c = 0; c < 3; c++) {
+                setTie(board[r][c]);
+              }  
+            }
+
+       }
+
+    }
+    void setWinner(JButton tile){
+        tile.setForeground(Color.green);
+        tile.setBackground(Color.gray);
+        textLabel.setText(currentplayer+ " à gagné!");
+    }
+
+    void setTie(JButton tile){
+        tile.setForeground(Color.orange);
+        tile.setBackground(Color.gray);
+        textLabel.setText("Match nul !");
+
     }
 }
